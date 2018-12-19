c43bb80280c12e316033df46cc2e7c5be300a1ad
hoeventl <hoeventl@rose-hulman.edu>
2018-12-19 16:01:21 -0500

    thru 5

--- src/m6_fancy_iterating.py
+++ src/m6_fancy_iterating.py
@@ -14,8 +14,8 @@ is different from:
 
 Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher,
          Mark Hays, Amanda Stouder, Aaron Wilkin, their colleagues,
-         and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         and Thomas Hoevener.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import rosegraphics as rg
 
@@ -23,7 +23,7 @@ import rosegraphics as rg
 def main():
     """ Calls the   TEST   functions in this module. """
     # -------------------------------------------------------------------------
-    # TODO: 2.  EXAMINE the  4  sequences immediately below
+    # DONE: 2.  EXAMINE the  4  sequences immediately below
     #   this comment, and READ the instructions that follows them.
     #
     # When you have examined the 4 sequences below and understand how
@@ -49,12 +49,12 @@ def main():
     #        RE-commenting out the previous test if you wish.
     # -------------------------------------------------------------------------
 
-    run_test_print_all_items_forwards(sequence1, sequence2, sequence3,
-                                      sequence4)
+    # run_test_print_all_items_forwards(sequence1, sequence2, sequence3,
+    #                                   sequence4)
     # run_test_print_all_items_backwards(sequence1, sequence2, sequence3,
     #                                    sequence4)
-    # run_test_print_items_at_odd_indices(sequence1, sequence2, sequence3,
-    #                                     sequence4)
+    run_test_print_items_at_odd_indices(sequence1, sequence2, sequence3,
+                                        sequence4)
     # run_test_print_items_in_second_half(sequence1, sequence2, sequence3,
     #                                     sequence4)
     # run_test_print_items_that_are_bigger_than_5()  # Uses different sequences
@@ -368,9 +368,12 @@ def print_all_items_forwards(sequence):
        Point at (90, 25)
     """
     # -------------------------------------------------------------------------
-    # TODO: 3. Implement and test this function.
+    # DONE: 3. Implement and test this function.
     #          Tests have been written for you (above).
     # -------------------------------------------------------------------------
+
+    for item in sequence:
+        print(item)
 
 
 ###############################################################################
@@ -390,9 +393,11 @@ def print_all_items_backwards(sequence):
        55
     """
     # -------------------------------------------------------------------------
-    # TODO: 4. Implement and test this function.
+    # DONE: 4. Implement and test this function.
     #          Tests have been written for you (above).
     # -------------------------------------------------------------------------
+    for item in range(len(sequence) - 1, -1, -1):
+        print(sequence[item])
 
 
 ###############################################################################
@@ -412,9 +417,11 @@ def print_items_at_odd_indices(sequence):
       33 is at index 5
     """
     # -------------------------------------------------------------------------
-    # TODO: 5. Implement and test this function.
+    # DONE: 5. Implement and test this function.
     #          Tests have been written for you (above).
     # -------------------------------------------------------------------------
+    for k in range(1, len(sequence), 2):
+        print(sequence[k], 'is at index', k)
 
 
 ###############################################################################
